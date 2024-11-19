1ce483950cc163bc9ca6936462b3b4190ca4d201
kimhappy <kimsvrkimsvr@gmail.com>
2024-11-19 14:50:07 +0900

    ref: scope as const

--- src/route/login.rs
+++ src/route/login.rs
@@ -3,13 +3,14 @@ use crate::{ util, env };
 
 #[rocket::get("/login")]
 pub fn get_login() -> Result< Redirect, String > {
-    let scope  = "user-read-private user-read-email";
+    const SCOPE: &str = "user-read-private user-read-email";
+
     let state  = util::random_string(16);
     let params = [
         ("response_type", "code"                   ),
         ("client_id"    , &env::CLIENT_ID          ),
         ("redirect_uri" , &env::REDIRECT_SERVER_URI),
-        ("scope"        , scope                    ),
+        ("scope"        , SCOPE                    ),
         ("state"        , &state                   )
     ];
     let qs           = serde_urlencoded::to_string(params).unwrap();
