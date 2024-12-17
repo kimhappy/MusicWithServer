6384c4adbe4bdc34baf71d1b922c628bd47b0bdc
kimhappy <kimhappyofficial@gmail.com>
2024-12-17 22:30:03 +0900

    ref: remove unused derive

--- src/lyric/lyric.rs
+++ src/lyric/lyric.rs
@@ -1,6 +1,6 @@
 use serde::{ Serialize, Deserialize };
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Lyric {
     pub begin  : f64,
     pub content: String
